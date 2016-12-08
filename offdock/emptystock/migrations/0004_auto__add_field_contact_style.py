673263ddc18fc8a5a18eac439e8578aac20c3852
314casso <picasso75@yandex.ru>
2016-12-08 11:43:08 +0300

    beta

---
+++ offdock/emptystock/migrations/0004_auto__add_field_contact_style.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding field 'Contact.style'
+        db.add_column('emptystock_contact', 'style',
+                      self.gf('django.db.models.fields.CharField')(max_length=50, null=True, blank=True),
+                      keep_default=False)
+
+
+    def backwards(self, orm):
+        # Deleting field 'Contact.style'
+        db.delete_column('emptystock_contact', 'style')
+
+
+    models = {
+        'emptystock.contact': {
+            'Meta': {'unique_together': "(('contact', 'type'),)", 'object_name': 'Contact'},
+            'contact': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'person': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['emptystock.Person']"}),
+            'style': ('django.db.models.fields.CharField', [], {'max_length': '50', 'null': 'True', 'blank': 'True'}),
+            'type': ('django.db.models.fields.IntegerField', [], {'default': '2'})
+        },
+        'emptystock.person': {
+            'Meta': {'object_name': 'Person'},
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'foto': ('django.db.models.fields.files.ImageField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '50', 'null': 'True', 'blank': 'True'}),
+            'middle_name': ('django.db.models.fields.CharField', [], {'max_length': '50', 'null': 'True', 'blank': 'True'}),
+            'position': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'})
+        }
+    }
+
+    complete_apps = ['emptystock']
