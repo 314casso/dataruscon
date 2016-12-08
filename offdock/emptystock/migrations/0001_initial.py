73045613e98ca30fb97638f4673ec9f25da9ee5a
314casso <picasso75@yandex.ru>
2016-12-08 13:52:26 +0300

    fix

--- offdock/emptystock/migrations/0001_initial.py
+++ offdock/emptystock/migrations/0001_initial.py
@@ -16,23 +16,16 @@ class Migration(SchemaMigration):
             ('middle_name', self.gf('django.db.models.fields.CharField')(max_length=50, null=True, blank=True)),
             ('position', self.gf('django.db.models.fields.CharField')(max_length=100, null=True, blank=True)),
             ('foto', self.gf('django.db.models.fields.files.ImageField')(max_length=100, null=True, blank=True)),
+            ('is_active', self.gf('django.db.models.fields.BooleanField')(default=True)),
         ))
         db.send_create_signal('emptystock', ['Person'])
-
-        # Adding M2M table for field contacts on 'Person'
-        m2m_table_name = db.shorten_name('emptystock_person_contacts')
-        db.create_table(m2m_table_name, (
-            ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True)),
-            ('person', models.ForeignKey(orm['emptystock.person'], null=False)),
-            ('contact', models.ForeignKey(orm['emptystock.contact'], null=False))
-        ))
-        db.create_unique(m2m_table_name, ['person_id', 'contact_id'])
 
         # Adding model 'Contact'
         db.create_table('emptystock_contact', (
             ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
             ('contact', self.gf('django.db.models.fields.CharField')(max_length=50)),
             ('type', self.gf('django.db.models.fields.IntegerField')(default=2)),
+            ('person', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['emptystock.Person'])),
         ))
         db.send_create_signal('emptystock', ['Contact'])
 
@@ -47,9 +40,6 @@ class Migration(SchemaMigration):
         # Deleting model 'Person'
         db.delete_table('emptystock_person')
 
-        # Removing M2M table for field contacts on 'Person'
-        db.delete_table(db.shorten_name('emptystock_person_contacts'))
-
         # Deleting model 'Contact'
         db.delete_table('emptystock_contact')
 
@@ -59,14 +49,15 @@ class Migration(SchemaMigration):
             'Meta': {'unique_together': "(('contact', 'type'),)", 'object_name': 'Contact'},
             'contact': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'person': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['emptystock.Person']"}),
             'type': ('django.db.models.fields.IntegerField', [], {'default': '2'})
         },
         'emptystock.person': {
             'Meta': {'object_name': 'Person'},
-            'contacts': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['emptystock.Contact']", 'symmetrical': 'False'}),
             'first_name': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
             'foto': ('django.db.models.fields.files.ImageField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
             'last_name': ('django.db.models.fields.CharField', [], {'max_length': '50', 'null': 'True', 'blank': 'True'}),
             'middle_name': ('django.db.models.fields.CharField', [], {'max_length': '50', 'null': 'True', 'blank': 'True'}),
             'position': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'})
