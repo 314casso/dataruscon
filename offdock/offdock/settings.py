2ee37bd143eace8988fb32e81a2b977a377fcea3
314casso <picasso75@yandex.ru>
2016-12-15 11:32:10 +0300

    fix

--- offdock/offdock/settings.py
+++ offdock/offdock/settings.py
@@ -20,7 +20,7 @@ from local_settings import * #@UnusedWildImport
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.4/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'data.ruscon.global']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
