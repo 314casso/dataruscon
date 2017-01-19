4c512144ad98de71a3794bd4b34ad025b0151630
314casso <picasso75@yandex.ru>
2017-01-19 09:49:06 +0300

    fix

---
+++ offdock/emptystock/migrations/0009_auto__chg_field_sms_date.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+
+        # Changing field 'Sms.date'
+        db.alter_column('emptystock_sms', 'date', self.gf('django.db.models.fields.DateTimeField')())
+
+    def backwards(self, orm):
+
+        # Changing field 'Sms.date'
+        db.alter_column('emptystock_sms', 'date', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True))
+
+    models = {
+        'emptystock.contact': {
+            'Meta': {'unique_together': "(('contact', 'type'),)", 'object_name': 'Contact'},
+            'contact': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'person': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['emptystock.Person']"}),
+            'type': ('django.db.models.fields.IntegerField', [], {'default': '2'})
+        },
+        'emptystock.person': {
+            'Meta': {'object_name': 'Person'},
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'foto': ('django.db.models.fields.files.ImageField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '50', 'null': 'True', 'blank': 'True'}),
+            'middle_name': ('django.db.models.fields.CharField', [], {'max_length': '50', 'null': 'True', 'blank': 'True'}),
+            'position': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'})
+        },
+        'emptystock.sms': {
+            'Meta': {'object_name': 'Sms'},
+            'agtid': ('django.db.models.fields.CharField', [], {'max_length': '50', 'null': 'True', 'blank': 'True'}),
+            'date': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'blank': 'True'}),
+            'http_code': ('django.db.models.fields.CharField', [], {'max_length': '50', 'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'inbox': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'rescount': ('django.db.models.fields.CharField', [], {'max_length': '50', 'null': 'True', 'blank': 'True'}),
+            'sender': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'smsid': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '50', 'db_index': 'True'}),
+            'status': ('django.db.models.fields.IntegerField', [], {'default': '1', 'db_index': 'True', 'blank': 'True'}),
+            'target': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'text': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'xml_response': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'})
+        }
+    }
+
+    complete_apps = ['emptystock']
